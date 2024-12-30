88eea694f844fdc0c59a3f9a554c8598ba53eb1c
Joana Noites <joanalune@gmail.com>
2024-12-30 22:05:27 +0000

    Board example

--- src/game.pl
+++ src/game.pl
@@ -3,3 +3,19 @@ game(Input) :-
 
 
 initial_state(0, _, _, []).
+
+
+/*
+
+board([
+    [X, X, X, X, X, X, X, X],
+    [X, 0, black , white , 0, 0, 0, black ],
+    [X, white , 0, black , black , 0, 0, white ],
+    [X, 0, black , 0, white , 0, black , 0],
+    [black , 0,  white , white , 0, black, 0, X],
+    [X, 0, 0, black , 0, 0, white , X],
+    [X,  white , 0, 0,  black ,  white , 0,  black],
+    [X,  white , 0, 0,  black ,  white , 0,  black],
+    [X, X, X, X, X, X, X, X]
+]).
+*/
