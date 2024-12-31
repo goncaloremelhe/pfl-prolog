5ed7c609eaf6beab6a64411fc182f23872f47b92
Joana Noites <joanalune@gmail.com>
2024-12-31 12:17:47 +0000

    More comments

--- src/board.pl
+++ src/board.pl
@@ -1,16 +1,3 @@
-/*
-board_example([
-    [X, X, X, X, X, X, X, X],
-    [X, 0, black , white , 0, 0, 0, black ],
-    [X, white , 0, black , black , 0, 0, white ],
-    [X, 0, black , 0, white , 0, black , 0],
-    [black , 0,  white , white , 0, black, 0, X],
-    [X, 0, 0, black , 0, 0, white , X],
-    [X,  white , 0, 0,  black ,  white , 0,  black],
-    [X,  white , 0, 0,  black ,  white , 0,  black],
-    [X, X, X, X, X, X, X, X]
-]).
-*/
 
 
 % ----------- generate_board(+Size, -Board)
--- src/utils.pl
+++ src/utils.pl
@@ -1,6 +1,11 @@
+% ----------- read_number(+X)
+% Reads number input from user and converts it to an integer, X is unified with said integer.
 read_number(X) :-
     read_number_aux(0,false,X).
 
+
+% ----------- read_number_aux(+Acc, +HasReadDigit, -X)
+% A helper predicate for read_number/1 that reads input character by character.
 read_number_aux(Acc,_,X):-
     get_code(C),
     C >= 48,
@@ -10,20 +15,23 @@ read_number_aux(Acc,_,X):-
     read_number_aux(Acc1,true,X).
 read_number_aux(X,true,X).
 
-
+% ----------- valid_input(+Options, -Value)
+% Ensures that the users input is a valid option from a given list.
 valid_input(Options,Value) :-
     read_number(Value),
     member(Value, Options).
 
+% Repeats unitl a valid input is entered
 valid_input(Options, Value) :-
     repeat,
     write('Nao foi desta :(\nPodes tentar outra:\n'),
     read_number(Value),
     member(Value,Options),
     !.
-
+% ----------- between(+Min, +Max, -Value)
+% Generates values between Min and Max, inclusive.
 between(Min, Max, Min):- Min =< Max.
 between(Min, Max, Value):-
     Min < Max,
-    NextMin is Min + 1,
-    between(NextMin, Max, Value).
+    NewMin is Min + 1,
+    between(NewMin, Max, Value).
